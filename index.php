14872f19cf398cfc7003095576efc1c1b6a4d112
Éctor Rodrigues <ectorrodrigues@gmail.com>
2018-03-24 02:47:32 -0300

    v2.1.1

--- index.php
+++ index.php
@@ -6,6 +6,11 @@
 */
 include ('app'.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.php');
 
+/** ______________________________________________________________________________________________________________
+*
+* Database file include
+*/
+include('app/config/database.php');
 
 
 /** ______________________________________________________________________________________________________________
@@ -15,50 +20,65 @@ include ('app'.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.php');
 
 //APPMODEL -------------------------------------------------------------------------------------
 
-if($auto_update_appmodel == 'yes'){
+//Starts with a verification of the last update. It only proceed if the last updade was done more than 1 hour ago, otherwise it will not update. I is an hourly update.
 
-	if(file_exists(MODEL_DIR.'AppModel.php')){
+$conn = db();
+$query = $conn->prepare("SELECT time FROM update_time_control ORDER BY id DESC limit 1"); 
+$query->execute();
+$last_update_time = $query->fetchColumn();
+$time_now = date("Y-m-d H:i:s");
+$time_allowed = date('Y-m-d H:i:s',strtotime('+1 hour',strtotime($last_update_time)));
 
-		$appmodel_local = file_get_contents(MODEL_DIR.'AppModel.php');
-		$appmodel = file_get_contents('https://raw.githubusercontent.com/ectorrodrigues/blackholeframe/master/app/model/AppModel.php');
 
-		if(empty($appmodel_local)){
-			$appmodel = str_replace(array("<pre>", "</pre>"), array("<?php", "?>" ), $appmodel);
-			file_put_contents(MODEL_DIR.'AppModel.php', $appmodel);
-		}
-		else{
-			preg_match_all("'function (.*?)\('si", $appmodel_local, $functions); 
-			$functions = $functions[1];	
+if($time_now > $time_allowed) {
 
-			$function_preg_content = array();
-			foreach ($functions as $functions_name) {
-				if(strpos($appmodel_local, $functions_name) == true){
-					preg_match("'function ".$functions_name."(.*?)endfunction'si", $appmodel, $function_preg);
-					$function_preg_content = $function_preg[0];
-					$appmodel = str_replace($function_preg_content, '', $appmodel);
+	if($auto_update_appmodel == 'yes'){
+
+		if(file_exists(MODEL_DIR.'AppModel.php')){
+
+			$appmodel_local = file_get_contents(MODEL_DIR.'AppModel.php');
+			$appmodel = file_get_contents('https://raw.githubusercontent.com/ectorrodrigues/blackholeframe/master/app/model/AppModel.php');
+
+			if(empty($appmodel_local)){
+				$appmodel = str_replace(array("<pre>", "</pre>"), array("<?php", "?>" ), $appmodel);
+				file_put_contents(MODEL_DIR.'AppModel.php', $appmodel);
+			}
+			else{
+				preg_match_all("'function (.*?)\('si", $appmodel_local, $functions); 
+				$functions = $functions[1];	
+
+				$function_preg_content = array();
+				foreach ($functions as $functions_name) {
+					if(strpos($appmodel_local, $functions_name) == true){
+						preg_match("'function ".$functions_name."(.*?)endfunction'si", $appmodel, $function_preg);
+						$function_preg_content = $function_preg[0];
+						$appmodel = str_replace($function_preg_content, '', $appmodel);
+					}
 				}
+
+				$appmodel = str_replace(array("<pre>", "</pre>"), array("<?php", "?>" ), $appmodel);
+				file_put_contents(MODEL_DIR.'AppModel.php', $appmodel, FILE_APPEND | LOCK_EX);
 			}
 
-			$appmodel = str_replace(array("<pre>", "</pre>"), array("<?php", "?>" ), $appmodel);
-			file_put_contents(MODEL_DIR.'AppModel.php', $appmodel, FILE_APPEND | LOCK_EX);
 		}
 
 	}
 
-}
 
 //ADMINMODEL -------------------------------------------------------------------------------------
 
-if($auto_update_adminmodel == 'yes'){
+	if($auto_update_adminmodel == 'yes'){
 
-	if(file_exists(MODEL_DIR.'AdminModel.php')){
+		if(file_exists(MODEL_DIR.'AdminModel.php')){
 
-		$appmodel_local = file_get_contents(MODEL_DIR.'AppModel.php');
-		$appmodel = file_get_contents('https://raw.githubusercontent.com/ectorrodrigues/blackholeframe/master/app/model/AdminModel.php');
+			$adminmodel_local = file_get_contents(MODEL_DIR.'AdminModel.php');
+			$adminmodel = file_get_contents('https://raw.githubusercontent.com/ectorrodrigues/blackholeframe/master/app/model/AdminModel.php');
 
-		if(empty($appmodel_local)){
-			$appmodel = str_replace(array("<pre>", "</pre>"), array("<?php", "?>" ), $appmodel);
-			file_put_contents(MODEL_DIR.'AppModel.php', $appmodel);
+			if(empty($adminmodel_local)){
+				$adminmodel = str_replace(array("<pre>", "</pre>"), array("<?php", "?>" ), $adminmodel);
+				file_put_contents(MODEL_DIR.'AdminModel.php', $adminmodel);
+			}
+
 		}
 
 	}
@@ -69,9 +89,8 @@ if($auto_update_adminmodel == 'yes'){
 
 /** ______________________________________________________________________________________________________________
 *
-* Database and Model includes
+* Model include
 */
-include('app/config/database.php');
 include('app/model/AppModel.php');
 
 
