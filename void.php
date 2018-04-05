04e579439f3405e2852b94d8b89d91e5d00dc3df
Éctor Rodrigues <ectorrodrigues@gmail.com>
2018-04-04 21:45:31 -0300

    v6.2
    void changes

--- void.php
+++ void.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-<link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,400i,500,600,700,700i,900" rel="stylesheet">
-<script
-  src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-  integrity="sha256-3edrmyuQ0w65f8gfBsqowzjJe2iM6n0nKciPUp8y+7E="
-  crossorigin="anonymous"></script>
+	<!-- Jquery and Ajax Libraries -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    
+    <!-- Bootstrap -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>   
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,400i,500,600,700,700i,900" rel="stylesheet">
 
 <title>Black Hole Framework</title>
 
@@ -21,14 +25,13 @@
 	}
 
 	.row{
+		position: relative;
 		width:100%;
-		padding:100px 0 5px 0;
 	}
 
-	.col{
-		width:300px;
-		text-align: left;
-		margin: 25px 0;
+	.vertical-align{
+  		display: flex;
+    	align-items:center;
 	}
 
 	a{
@@ -75,28 +78,50 @@
 	<div class="container-fluid" align="center">
 
 		<div class="row" align="center">
-		
 			<div class="col">
-				<h2>1.</h2>
 				At beggining there was the void.<br>
 				<strong>Start the Big Bang.</strong><br><br>
+			</div>
+		</div>
+
+		<div class="row vertical-align" align="center">
+			<div class="col-lg-2 offset-lg-2 text-right">
+				<h2>1.</h2>
+			</div>
+			<div class="col-lg-8 text-left">
+				First of all, make sure you had created a local directory for your project, on your local server.
+			</div>
+		</div>
+
+		<div class="row vertical-align" align="center">
+			<div class="col-lg-2 offset-lg-2 text-right">
+				<h2>2.</h2>
+			</div>
+			<div class="col-lg-8 text-left">
 				<input type="text" class="sitename" value="" placeholder="what's your website's name" /><br>
 				<small>
 					*Type the same as your wamp project<br>
-					e.g. <strong>localhost/mysite</strong> > <strong>"mysite"</strong> is your website's name.<br>
+					e.g. <strong>localhost/mysite</strong> > <strong>"mysite"</strong> is your website's name. 
 					(no special chars allowed)
 				</small>
 			</div>
-
-			<div class="col">
-				<h2>2.</h2>
+		</div>
+			
+		<div class="row vertical-align" align="center">
+			<div class="col-lg-2 offset-lg-2 text-right">
+				<h2>3.</h2>
+			</div>
+			<div class="col-lg-8 text-left">
 				<a href="http://mova.ppg.br/resources/blackholeframe/singularity.php"><strong>Right Click Here and Save As</strong></a><br/>
 				<small>*"Save As" to your localhost site's root</small>
 			</div>
+		</div>
 		
-
-			<div class="col pointer" onclick="gotoroot()">
-				<h2>3.</h2>
+		<div class="row vertical-align" align="center">
+			<div class="col-lg-2 offset-lg-2 text-right pointer" onclick="gotoroot()">
+				<h2>4.</h2>
+			</div>
+			<div class="col-lg-8 text-left">
 				After download the file, <strong> click here to begin.</strong>
 			</div>
 
