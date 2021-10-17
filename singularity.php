d637fd56d1180035fbba161acf2cbf8e963372e6
Éctor Rodrigues <ectorrodrigues@gmail.com>
2021-10-17 01:43:03 -0300

    Refactoring NEW VERSION

--- singularity.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$file_url = 'https://raw.githubusercontent.com/ectorrodrigues/blackholeframe/master/bigbang.php';
-	header('Content-Type: application/octet-stream');
-	header("Content-Transfer-Encoding: Binary"); 
-	header("Content-disposition: attachment; filename=\"" . basename($file_url) . "\""); 
-	readfile($file_url);
-?>
