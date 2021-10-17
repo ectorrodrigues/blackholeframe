772a3535b9095eeb28ccad9a20f1a309b802dc8f
Éctor Rodrigues <ectorrodrigues@gmail.com>
2021-10-17 02:07:08 -0300

    create database

--- creator/model/AppModel.php
+++ creator/model/AppModel.php
@@ -87,7 +87,7 @@
 if ($page == 'new') {
     try {
         try {
-            $pdo = new PDO("mysql:host=localhost;", "root", "");
+            $pdo = new PDO("mysql:host=localhost;", "root", "root");
             // Set the PDO error mode to exception
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
